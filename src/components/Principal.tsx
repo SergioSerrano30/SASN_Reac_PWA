48a9b0491179266eda505aec535e3d32bd6a55d8
SergioSerrano30 <84033208+SergioSerrano30@users.noreply.github.com>
2022-10-13 01:37:32 -0600

    Pantalla principal con una imagen

---
+++ src/components/Principal.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export const Principal = () => {
+  return (
+    <div>
+      <h3>Principal</h3>
+      <button className="btn btn-primary">Bienvenido</button>
+      <br />
+      <img
+        src="https://okdiario.com/guiltybit/wp-content/uploads/2021/07/web-Dragon-Ball-Super-Super-Hero.jpg"
+        alt="goku"
+        style={{
+          width: 400,
+          height: 300
+        }}
+      />
+    </div>
+  );
+};
